04775eeb8d80826c840bd79fa8b29af98e209142
clown6910clown <tushar.garg.btech2023@sitpune.edu.in>
2025-03-02 18:44:31 +0530

    commit

--- src/DecoyDuck.java
+++ src/DecoyDuck.java
@@ -2,7 +2,7 @@ public class DecoyDuck extends Duck {
 
     public DecoyDuck() {
         this.quackBehaviour = new MuteQuack();// Doesn't quack
-        this.swimBehaviour = new Floating();
+        this.swimBehaviour = new Sink();
         this.flyBehaviour = new NoFly();        // Can't fly
     }
 
